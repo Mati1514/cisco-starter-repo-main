4b299244294e552be0869b03f8cbb96a7cf82e84
Mati1514 <52832682+Mati1514@users.noreply.github.com>
2023-04-11 02:45:53 -0400

    task 2

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+function Banner() {
+  return (
+    <div className="banner">
+      <div className="banner-overlay">
+        <div className="banner-content">
+          <h1>SEXTANT</h1>
+          <p>cisco</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Exhibit({ title, children }) {
+  return (
+    <div className="exhibit">
+      <h2 className="exhibit-title">{title}</h2>
+      <div className="exhibit-content">
+        {children}
+      </div>
+    </div>
+  );
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Wellcome to React
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Banner />        
+      <Exhibit title="Example Exhibit">
+        <p>Child component 1</p>
+        <p>Child component 2</p>
+        <p>Child component 3</p>
+      </Exhibit>
     </div>
   );
 }
